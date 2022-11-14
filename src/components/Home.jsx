329a3e75f482f0e7cdc6f2c1d044bff75959dc91
Adam Barron <adambarron10@btinternet.com>
2022-11-14 15:47:52 +0000

    Simplify schedule

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Countdown from './Countdown';
-
-const weddingCeremony = new Date("06/04/2023, 13:30");
-const weddingDrinksReception = new Date("06/04/2023, 14:30");
-const weddingBreakfast = new Date("06/04/2023, 15:30");
-const weddingEveningReception = new Date("06/04/2023, 19:30");
-const breakfast = new Date("06/05/2023, 7:30");
+import { CARRIAGES, CEREMONY, EVENING_RECEPTION, MORNING_BREAKFAST, MORNING_BREAKFAST_END } from '../helpers/schedule'
 
 const Home = () => (
     <div className="Home">
@@ -13,18 +8,15 @@ const Home = () => (
             <div className="row">
                 <div className="col">
                     <h1>Countdown to the ceremony</h1>
-                    <Countdown date={weddingCeremony}/>
+                    <Countdown date={CEREMONY}/>
                     <p>We would be honoured if you could join us both to share our special day with us. 
                         We hope this site will help answer any questions about you have about the day in advance.</p>
-                    <h2>Schedule</h2>
-                    <h3>{weddingCeremony.toDateString()}</h3>
-                    <p>Ceremony: {weddingCeremony.toTimeString()}</p>
-                    <p>Drinks Reception: {weddingDrinksReception.toTimeString()}</p>
-                    <p>Wedding Breakfast: {weddingBreakfast.toTimeString()}</p>
-                    <p>Evening Reception: {weddingEveningReception.toTimeString()}</p>
-                    <p></p>
-                    <h3>{breakfast.toDateString()}</h3>
-                    <p>Breakfast: {breakfast.toTimeString()}</p>
+                    <h2>The day: {CEREMONY.toDateString()}</h2>
+                    <p>Ceremony: {CEREMONY.toTimeString()}</p>
+                    <p>Evening Reception: {EVENING_RECEPTION.toTimeString()} - {CARRIAGES.toTimeString()}</p>
+                    <h2>The morning after: {MORNING_BREAKFAST.toDateString()}</h2>
+                    <p>For those staying at Swinton, please join us for:</p>
+                    <p>Breakfast: {MORNING_BREAKFAST.toTimeString()} - {MORNING_BREAKFAST_END.toTimeString()}</p>
                     <h2>Gifts</h2>
                     <p>We have been fortunate enough to live together for some time now, 
                         and as a result would love any contribution towards a housing deposit, 
---
+++ src/helpers/schedule.js
@@ -0,0 +1,8 @@
+export const CEREMONY = new Date("06/04/2023, 13:30");
+export const DRINKS_RECEPTION = new Date("06/04/2023, 14:30");
+export const WEDDING_BREAKFAST = new Date("06/04/2023, 15:30");
+export const EVENING_RECEPTION = new Date("06/04/2023, 19:30");
+export const CARRIAGES = new Date("06/05/2023, 00:00");
+export const MORNING_BREAKFAST = new Date("06/05/2023, 7:30");
+export const MORNING_BREAKFAST_END = new Date("06/05/2023, 10:00");
+export const CHECKOUT = new Date("06/05/2023, 11:00");
