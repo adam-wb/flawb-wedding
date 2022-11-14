123f91a5f7e808c0676bd20da350c54f0471edd5
Adam Barron <adambarron10@btinternet.com>
2022-11-14 16:55:18 +0000

    Add more accomodation links

--- src/components/Accomodation.jsx
+++ src/components/Accomodation.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { AIRBNB_SEARCH, TRIP_ADVISOR_SEARCH, VISIT_MASHAM_ACCOMODATION } from '../helpers/links';
 
 const Accomodation = () => (
     <div className="container">
         <div className="row">
             <div className="col">
                 <h1>Accomodation</h1>
-                <p>We have secured exclusive use of Swinton Park Hotel for the day of the reception, 
-                    as a result it is possible to book one of 32 rooms at Swinton through us should you wish. 
-                    Alternatively, we reccommend one of a number of hotels around Masham and the surrounding area.</p>
+                <h2>Swinton</h2>
+                <p>We have secured exclusive use of the beautiful Swinton Park Hotel for the day of the reception, 
+                    as a result it is possible to book a room at Swinton for the night of the 4th June through us should you wish.
+                    Each room at Swinton will cost £150; this also includes breakfast the next morning.
+                </p>
+                <h2>Hotels</h2>
+                <a href={TRIP_ADVISOR_SEARCH}>TripAdvisor</a>
+                <p>Alternatively, we reccommend one of a number of hotels around Masham and the surrounding area.
+                    There are a few hotels in Masham itself, but accomodation in places like Bedale or Ripon would also give just a short drive to the venue.
+                    You can find more accomodation that might not be on places like TripAdvisor on the <a href={VISIT_MASHAM_ACCOMODATION}>Visit Masham</a> site.
+                </p>
+                <h2>AirBnB</h2>
+                <a href={AIRBNB_SEARCH}>AirBnB</a>
+                <p>There are numerous AirBnB properties around Masham, often in very nice old houses.</p>
             </div>
         </div>
     </div>
-
 )
 
 export default Accomodation;
---
+++ src/helpers/links.js
@@ -0,0 +1,3 @@
+export const TRIP_ADVISOR_SEARCH = "https://www.tripadvisor.co.uk/Hotels-g670891-a_ufe.true-Masham_North_Yorkshire_England-Hotels.html"
+export const AIRBNB_SEARCH = "https://www.airbnb.co.uk/s/Masham/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&price_filter_input_type=0&price_filter_num_nights=5&query=Masham&place_id=ChIJeQxTrTiofkgROTkHbaBf9LI&date_picker_type=calendar&checkin=2023-06-04&checkout=2023-06-05&source=structured_search_input_header&search_type=autocomplete_click"
+export const VISIT_MASHAM_ACCOMODATION = "https://www.visitmasham.com/directorycategory/accommodation/"
