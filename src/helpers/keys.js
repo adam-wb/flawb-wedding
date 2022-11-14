1ea71267b73f7ecbac6c28db86ffa5a93b680020
Adam Barron <adambarron10@btinternet.com>
2022-11-14 17:12:54 +0000

    Update API key again

--- src/helpers/keys.js
+++ src/helpers/keys.js
@@ -1,2 +1,2 @@
 // API key is restricted to prevent use by other websites
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyDpUMGR-3Xjvmhh4uV90PAjMzReEKDm1bE';
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDFxS8quJDXvnbTUiIWAqrXso6unA30EPc';
