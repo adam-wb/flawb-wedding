acd6d89f6394380df246b71b58916c044ca65390
Adam Barron <adambarron10@btinternet.com>
2022-12-24 11:15:50 +0000

    Add venue photos

--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Map from './Map'
 import * as locations from '../helpers/locations';
+import VenuePhoto from './VenuePhoto';
+
+import swintonPath from '../images/swinton.jpg';
+import churchPath from '../images/church.png';
 
 const Venue = () => {
     const ceremony = locations.ST_ANDREWS;
@@ -18,12 +22,18 @@ const Venue = () => {
                                 <p>The ceremony will take place at the church in Fabiana's home village.</p>
                                 <p>{ceremony.address}</p>
                             </div>
+                            <div className="col">
+                                <VenuePhoto alt="Photo of St.Andrew's Church" path={churchPath}></VenuePhoto>
+                            </div>
                         </div>
                         <div className="row">
                             <h1>Reception - Swinton Park Hotel</h1>
                             <div className="col">
                                 <p>The reception takes place at Swinton Park, just outside the market town of Masham.</p>
                                 <p>{reception.address}</p>
+                            </div>
+                            <div className="col">
+                                <VenuePhoto alt="Photo of Swinton Estate" path={swintonPath}></VenuePhoto>
                             </div>
                         </div>
                         <div className="row">
---
+++ src/components/VenuePhoto.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VenuePhoto = ({alt, path}) => (
+    <div className="VenuePhoto">
+        <img src={path} alt={alt}></img>
+    </div>
+)
+
+export default VenuePhoto;
