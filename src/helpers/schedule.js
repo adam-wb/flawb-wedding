83ba3b0c7f1631e593c0bd1621ed17e642db8bf4
Adam Barron <adambarron10@btinternet.com>
2023-05-16 21:17:50 +0100

    Update timings for the day

--- src/helpers/schedule.js
+++ src/helpers/schedule.js
@@ -1,6 +1,6 @@
 export const CEREMONY = new Date("06/04/2023, 13:30");
-export const DRINKS_RECEPTION = new Date("06/04/2023, 14:30");
-export const WEDDING_BREAKFAST = new Date("06/04/2023, 15:30");
+export const DRINKS_RECEPTION = new Date("06/04/2023, 15:00");
+export const WEDDING_BREAKFAST = new Date("06/04/2023, 16:15");
 export const EVENING_RECEPTION = new Date("06/04/2023, 19:30");
 export const CARRIAGES = new Date("06/05/2023, 00:00");
 export const MORNING_BREAKFAST = new Date("06/05/2023, 7:30");
